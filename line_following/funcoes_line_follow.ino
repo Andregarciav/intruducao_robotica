9cf811148d34dad9ad5d7aa9ccb78d0f99daea7f
HenriqueCheik <henriquecheik@gmail.com>
2017-06-06 18:48:33 -0300

    Add files via upload

---
+++ line_following/funcoes_line_follow.ino
@@ -0,0 +1,79 @@
+#define num_medias 20
+#define tolerancia_ldr 10
+
+
+int calib_line_follow[2]; //PRIMEIRA POSICAO CALIBRACAO ESQUERDA, SEGUNDA POSICAO CALIB DIREITA
+
+int mediaLeituras (int vezes, int pin){
+  int sum = 0;
+  int leitura;
+
+  for(int i = 0; i < vezes; i++){
+    leitura = analogRead(pin);
+    sum += leitura;
+    delay(10);
+  }
+  return (sum / vezes);
+}
+
+void calibra_line_follow(){
+  //Calibra os LDRs de Line Follow
+
+    //LDR L
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Calibrando");
+  lcd.setCursor(0,1);
+  lcd.print("LDR L");
+  delay(2000);
+  calib_line_follow[0] = mediaLeituras(num_medias, LDR_L);
+  calib_line_follow[0] *= (1.0 + (tolerancia_ldr/100.0));
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Calib LDR L:");
+  lcd.setCursor(0,1);
+  lcd.print(calib_line_follow[0]);
+  delay(5000);
+
+  //LDR L
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Calibrando");
+  lcd.setCursor(0,1);
+  lcd.print("LDR R");
+  delay(2000);
+  calib_line_follow[1] = mediaLeituras(num_medias, LDR_R);
+  calib_line_follow[1] *= (1.0 + (tolerancia_ldr/100.0));
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Calib LDR R:");
+  lcd.setCursor(0,1);
+  lcd.print(calib_line_follow[1]);
+  delay(5000);
+}
+
+void line_follow(){
+  motor1->run(FORWARD);
+  motor2->run(FORWARD);
+  motor1->setSpeed(mot1);
+  motor2->setSpeed(mot2);
+
+  int reduz1 = mot1 * 0.8;
+  int reduz2 = mot2 * 0.8;
+  while(1){
+    valor_LDR_R = analogRead(LDR_R);
+    valor_LDR_L = analogRead(LDR_L);
+    if(valor_LDR_L < calib_line_follow[0]){
+      motor1->setSpeed(reduz1);
+      while(valor_LDR_L < calib_line_follow[0]);
+      motor1->setSpeed(mot1);
+    }
+    if(valor_LDR_R < calib_line_follow[1]){
+      motor2->setSpeed(reduz2);
+      while(valor_LDR_R < calib_line_follow[1]);
+      motor2->setSpeed(mot2);
+    }
+
+  }
+}
+
---
+++ line_following/line_following.ino
@@ -0,0 +1,59 @@
+//inclusão de biblioteca LCD
+#include <LiquidCrystal.h>
+//Bilbioteca Shieldd
+#include <Adafruit_MotorShield.h>
+#include <Wire.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+//LDRs DE LINE FOLLOWING
+#define LDR_L 12
+#define LDR_R 14
+
+//DEFINIÇÃO DOS MOTORES
+#define mot1 250 //Potência motor 1
+#define mot2 222 // Potência motor 1
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *motor1 = AFMS.getMotor(1);    //chama motor 1 no rele 3
+Adafruit_DCMotor *motor2 = AFMS.getMotor(2);    //chama motor 2 no rele 4 MOTOR DOIS GIRO INVERTIDO
+
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+/////////////////////////////////////////////////////////////////////////
+
+int valor_LDR_L = 0;
+int valor_LDR_R = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  AFMS.begin();
+  pinMode(LDR_L, INPUT);
+  pinMode(LDR_R, INPUT);
+}
+
+void loop ()
+{
+  valor_LDR_R = analogRead(LDR_R);
+  valor_LDR_L = analogRead(LDR_L);
+
+  Serial.print("Valor Esquerdo");
+  Serial.print("\t");
+  Serial.print("Valor Direito");
+  Serial.println("");
+
+  Serial.print("\t");
+  Serial.print(valor_LDR_L);
+  Serial.print("\t");
+  Serial.print("\t");
+  Serial.print(valor_LDR_R);
+  Serial.println("");
+
+  delay(1000);                       
+
+  calibra_line_follow();
+}
+
+
